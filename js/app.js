cc562bc8b4454624813b2f34f94b03814ec4f586
luansantoskr <sluan380@gmail.com>
2021-07-03 11:27:09 -0300

    ajustando login e criando script

---
+++ js/app.js
@@ -0,0 +1,14 @@
+CadastroCliente("Sucesso!");
+
+function CadastroCliente(msg)
+{
+    var nome = document.getElementById("nome");
+    var cpf = document.getElementById("cpf");
+    var telefone = document.getElementById("telefone");
+    var cep = document.getElementById("cep");
+    var endereco = document.getElementById("endereco");
+    var numero = document.getElementById("numero");
+    var bairro = document.getElementById("bairro");   
+    
+}
+
